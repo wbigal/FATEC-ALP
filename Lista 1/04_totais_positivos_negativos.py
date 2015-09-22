8cdc21e5844bacf55c52f789a111a123def53510
Wagner Bigal <wagner.bigal@wal-mart.com>
2015-09-22 10:31:05 -0300

    Lista 1 exercício 4

--- Lista 1/04_totais_positivos_negativos.py
+++ Lista 1/04_totais_positivos_negativos.py
@@ -1,10 +1,17 @@
 #coding: utf-8
 
 numero = -1
+positivo = 0
+negativo = 0
 
 while(numero != 0):
   numero = int(input("Digite um número inteiro (zero para sair): "))
   if (numero > 0):
     print("{} é positivo").format(numero)
+    positivo += numero
   elif (numero < 0):
     print("{} é negativo").format(numero)
+    negativo += numero
+
+print("Total dos positivos: {}").format(positivo)
+print("Ttoal dos negativos: {}").format(negativo)
