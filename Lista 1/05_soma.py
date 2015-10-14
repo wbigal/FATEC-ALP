71f875fb02e595de93c1720172efa326e2fb188e
Wagner Bigal <wagner.bigal@wal-mart.com>
2015-10-14 13:42:17 -0300

    Exercício 05

---
+++ Lista 1/05_soma.py
@@ -0,0 +1,7 @@
+#coding: utf-8
+
+int_number = int(input("Digite um número inteiro: "))
+decimal_number = float(input("Digite um número real: "))
+total = int_number + decimal_number
+
+print("Total: {}").format(total)
